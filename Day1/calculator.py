8a49e0f25a60f8d1df86ede69d23422907541a17
Brandt <breutiman@gmail.com>
2019-07-01 12:49:32 -0600

    Example game stuff

---
+++ Day1/calculator.py
@@ -0,0 +1,19 @@
+''' Example Project -- Simple Calculator'''
+
+# 1. Output the phrase 'Hello, welcome to my simple calculator'
+print('Hello, welcome to my simple calculator')
+
+# 2. Get x & y from the user
+x = int(input('Please enter your first number: '))
+y = int(input('Please enter you second number: '))
+
+# 3. Print out x + y
+print("x+y is: " + str(x + y))
+# 4. Print out x - y
+print("x-y is: " + str(x-y))
+# 5. Print x * y
+print("x*y is: " + str(x*y))
+# 6. Print x / y
+print("x/y is: " + str(x/y))
+# 7. Print x ^ y
+print("x^y is: " + str(x**y))
